eb55c4c5e16ad7e8375b4348ebd8851b812dc5f0
Dev Mistry <90182112+DevM15@users.noreply.github.com>
2024-09-26 20:22:26 +0530

    Second commit

--- Product Store/Frontend/src/store/product.js
+++ Product Store/Frontend/src/store/product.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 
 export const useProductStore = create((set) => ({
-    product: [],
+    products: [],
     setProducts: (products) => set({products}),
     createProduct: async (newProduct) => {
         if(!newProduct.name || !newProduct.image || !newProduct.price){
@@ -15,7 +15,10 @@ export const useProductStore = create((set) => ({
             body:JSON.stringify(newProduct),
         });
         const data = await res.json();
-        set((state) => ({products:[...state.products, data.data]}))
+        set((state) => ({
+            products:[...state.products, data.data]
+            // products: state.products.map((product) => (product._id === pid ? data.data : product)),
+        }))
         return {success:true,message:"Product created successfully" }
     }
 }));
