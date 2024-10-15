0fdf5bef45061e6b38aa6dd33e4c2629f1e70ad5
Dev Mistry <90182112+DevM15@users.noreply.github.com>
2024-10-15 21:54:48 +0530

    First commit

---
+++ Word Scramble Game/script.js
@@ -0,0 +1,95 @@
+const words = [
+    {
+      word: "apple",
+      hint: "A fruit that keeps the doctor away."
+    },
+    {
+      word: "elephant",
+      hint: "The largest land animal."
+    },
+    {
+      word: "jupiter",
+      hint: "The largest planet in our solar system."
+    },
+    {
+      word: "guitar",
+      hint: "A musical instrument with strings."
+    },
+    {
+      word: "pyramid",
+      hint: "A structure built by ancient Egyptians."
+    },
+    {
+      word: "volcano",
+      hint: "A mountain that erupts with lava."
+    },
+    {
+      word: "kangaroo",
+      hint: "An animal that hops and carries babies in a pouch."
+    },
+    {
+      word: "python",
+      hint: "A large snake or a popular programming language."
+    },
+    {
+      word: "chess",
+      hint: "A board game of strategy with kings and queens."
+    },
+    {
+      word: "bicycle",
+      hint: "A vehicle with two wheels and pedals."
+    },
+    {
+      word: "internet",
+      hint: "A global network connecting millions of computers."
+    },
+    {
+      word: "rainbow",
+      hint: "An arc of colors seen after rain."
+    },
+    {
+      word: "mountain",
+      hint: "A large natural elevation of the earth's surface."
+    },
+    {
+      word: "penguin",
+      hint: "A flightless bird that lives in cold regions."
+    },
+    {
+      word: "robot",
+      hint: "A machine capable of carrying out complex tasks."
+    }
+  ];
+
+const wordText = document.querySelector(".word"),
+hintText = document.querySelector(".hint span"),
+refreshBtn = document.querySelector(".refresh-word"),
+checkBtn = document.querySelector(".check-word"),
+inputFeild = document.querySelector("input");
+
+let correctWord;
+
+const initGame = () => {
+    let randomObj = words[Math.floor(Math.random() * words.length)];
+    let wordArray = randomObj.word.split("");
+    for (let i = wordArray.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
+    }
+    wordText.innerHTML = wordArray.join("");
+    hintText.innerHTML = randomObj.hint;
+    correctWord = randomObj.word.toLowerCase();
+    console.log(randomObj);
+}
+initGame();
+
+const checkWord = () => {
+  let userWord =  inputFeild.value.toLocaleLowerCase();
+  if(!userWord) return alert('Please enter a word ')
+  if(userWord !== correctWord) return alert (`Opps! ${userWord} is not a correct word`);
+  alert (`Congrats! ${userWord.toUpperCase()} is a correct word`)
+} 
+
+refreshBtn.addEventListener("click", initGame);
+checkBtn.addEventListener("click", checkWord)
+  
